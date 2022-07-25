aa7169a476ff6d8660009eafaceabc27a9d6d236
juriaCardozo <ccardozojulia@gmail.com>
2022-07-25 20:29:38 -0300

    Classes ChessMatch, ChessPiece, Color and IU

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -2,9 +2,11 @@ package applications;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
